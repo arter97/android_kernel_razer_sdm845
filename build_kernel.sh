mcdachpappe/android_kernel_oneplus_sdm845

Add build tools
Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

build_kernel.sh
@@ -1,0 +1,69 @@
+#!/bin/bash
+export KERNELDIR=`readlink -f .`
+export RAMFS_SOURCE=`readlink -f $KERNELDIR/ramdisk`
+export PARTITION_SIZE=67108864
+
+echo "kerneldir = $KERNELDIR"
+echo "ramfs_source = $RAMFS_SOURCE"
+
+RAMFS_TMP="/tmp/arter97-op6-ramdisk"
+
+echo "ramfs_tmp = $RAMFS_TMP"
+cd $KERNELDIR
+
+if [ "${1}" = "skip" ] ; then
+	echo "Skipping Compilation"
+else
+	echo "Compiling kernel"
+	cp defconfig .config
+	make "$@" || exit 1
+fi
+
+echo "Building new ramdisk"
+#remove previous ramfs files
+rm -rf '$RAMFS_TMP'*
+rm -rf $RAMFS_TMP
+rm -rf $RAMFS_TMP.cpio
+#copy ramfs files to tmp directory
+cp -axpP $RAMFS_SOURCE $RAMFS_TMP
+cd $RAMFS_TMP
+
+#clear git repositories in ramfs
+find . -name .git -exec rm -rf {} \;
+find . -name EMPTY_DIRECTORY -exec rm -rf {} \;
+
+$KERNELDIR/ramdisk_fix_permissions.sh 2>/dev/null
+
+cd $KERNELDIR
+rm -rf $RAMFS_TMP/tmp/*
+
+cd $RAMFS_TMP
+find . | fakeroot cpio -H newc -o | gzip -9 > $RAMFS_TMP.cpio.gz
+ls -lh $RAMFS_TMP.cpio.gz
+cd $KERNELDIR
+
+echo "Making new boot image"
+mkbootimg \
+    --kernel $KERNELDIR/arch/arm64/boot/Image.gz-dtb \
+    --ramdisk $RAMFS_TMP.cpio.gz \
+    --cmdline 'androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true androidboot.usbcontroller=a600000.dwc3 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 buildvariant=user' \
+    --base           0x00000000 \
+    --pagesize       4096 \
+    --kernel_offset  0x00008000 \
+    --ramdisk_offset 0x01000000 \
+    --second_offset  0x00f00000 \
+    --tags_offset    0x00000100 \
+    --os_version     9.0.0 \
+    --os_patch_level 2018-09 \
+    --header_version 1 \
+    -o $KERNELDIR/boot.img
+
+GENERATED_SIZE=$(stat -c %s boot.img)
+if [[ $GENERATED_SIZE -gt $PARTITION_SIZE ]]; then
+	echo "boot.img size larger than partition size!" 1>&2
+	exit 1
+fi
+
+echo "done"
+ls -al boot.img
+echo ""

arter97/android_kernel_razer_sdm845

build_kernel.sh
@@ -1,0 +1,69 @@
+#!/bin/bash
+export KERNELDIR=`readlink -f .`
+export RAMFS_SOURCE=`readlink -f $KERNELDIR/ramdisk`
+export PARTITION_SIZE=67108864
+
+echo "kerneldir = $KERNELDIR"
+echo "ramfs_source = $RAMFS_SOURCE"
+
+RAMFS_TMP="/tmp/arter97-aura-ramdisk"
+
+echo "ramfs_tmp = $RAMFS_TMP"
+cd $KERNELDIR
+
+if [ "${1}" = "skip" ] ; then
+	echo "Skipping Compilation"
+else
+	echo "Compiling kernel"
+	cp defconfig .config
+	make "$@" || exit 1
+fi
+
+echo "Building new ramdisk"
+#remove previous ramfs files
+rm -rf '$RAMFS_TMP'*
+rm -rf $RAMFS_TMP
+rm -rf $RAMFS_TMP.cpio
+#copy ramfs files to tmp directory
+cp -axpP $RAMFS_SOURCE $RAMFS_TMP
+cd $RAMFS_TMP
+
+#clear git repositories in ramfs
+find . -name .git -exec rm -rf {} \;
+find . -name EMPTY_DIRECTORY -exec rm -rf {} \;
+
+$KERNELDIR/ramdisk_fix_permissions.sh 2>/dev/null
+
+cd $KERNELDIR
+rm -rf $RAMFS_TMP/tmp/*
+
+cd $RAMFS_TMP
+find . | fakeroot cpio -H newc -o | gzip -9 > $RAMFS_TMP.cpio.gz
+ls -lh $RAMFS_TMP.cpio.gz
+cd $KERNELDIR
+
+echo "Making new boot image"
+mkbootimg \
+    --kernel $KERNELDIR/arch/arm64/boot/Image.gz-dtb \
+    --ramdisk $RAMFS_TMP.cpio.gz \
+    --cmdline 'console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA84000 androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 buildvariant=user' \
+    --base           0x00000000 \
+    --pagesize       4096 \
+    --kernel_offset  0x00008000 \
+    --ramdisk_offset 0x01000000 \
+    --second_offset  0x00f00000 \
+    --tags_offset    0x00000100 \
+    --os_version     9.0.0 \
+    --os_patch_level 2019-02 \
+    --header_version 1 \
+    -o $KERNELDIR/boot.img
+
+GENERATED_SIZE=$(stat -c %s boot.img)
+if [[ $GENERATED_SIZE -gt $PARTITION_SIZE ]]; then
+	echo "boot.img size larger than partition size!" 1>&2
+	exit 1
+fi
+
+echo "done"
+ls -al boot.img
+echo ""